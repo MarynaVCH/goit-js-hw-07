826d1976473c7cede9f6dcee5429a8431a6b0ff6
MarynaVCH <83900520+MarynaVCH@users.noreply.github.com>
2021-10-19 15:18:52 +0300

    Create and render markup 02-lightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,20 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup();
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryMarkup() {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <a class="gallery__item" href="${preview}">
+  <img class="gallery__image" src="${original}" alt="${description}" />
+</a>
+      `;
+    })
+    .join('');
+}
